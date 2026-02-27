5b0ee63d3dd2a99f2d660a7a1d6a993a4a3bbd17
kang136 <kangmalual69@gmail.com>
2026-02-27 16:00:51 +0300

    Only small Adjustments I made to the code.

--- todo.js
+++ todo.js
@@ -2,12 +2,21 @@ const input = document.querySelector("#taskInput");
 const addBtn = document.querySelector("#addTaskButton");
 const deletebtn = document.querySelector("#deleteTaskButton");
 const taskList = document.querySelector("#taskList");
+const check = document.querySelector("#check");
 
 addBtn.addEventListener("click", function() {
     
     const taskText = input.value.trim();
     if (taskText) {
         const li = document.createElement("li");
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.classList.add("checkbox");
+        checkbox.id = "check";
+        const newLabel = document.createElement('label');
+        newLabel.htmlFor = check;
+         newLabel.appendChild(document.createTextNode( input.value));
+        li.appendChild(checkbox);
         li.textContent = taskText;
         taskList.appendChild(li);
         input.value = "";
